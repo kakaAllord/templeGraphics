7b96446273c2eda18c4b707c82fc35570675136a
kakaAllord <allordcodes@gmail.com>
2026-07-30 07:53:27 +0300

    Drawing a Triangle

--- templeGraphics/src/templeGraphics/Application.cpp
+++ templeGraphics/src/templeGraphics/Application.cpp
@@ -27,8 +27,7 @@ int main()
 {
 	templeG::Log::Init();
 
-	int success = glfwInit();
-	TG_ASSERT(success, "GLFW Init Failed!");
+	TG_ASSERT(glfwInit(), "GLFW Init Failed!");
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -50,14 +49,101 @@ int main()
 		return -1;
 	}
 
+	glViewport(0, 0, windowSpec.width, windowSpec.height);
 	glfwSetWindowSizeCallback(window, TGWindowSizeCallback);
+
+
+	//=========== All other code comes here ========================//
+
+	float vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	const char* vertexShaderSource =
+		"#version 330 core\n"
+		"layout (location=0) in vec3 aPos;\n"
+		"void main() {\n"
+		"gl_Position = vec4(aPos, 1.0f);\n"
+		"}\n";
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+	glCompileShader(vertexShader);
+
+	int success;
+	const int infoLog_L = 512;
+	char infoLog[infoLog_L];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, infoLog_L, NULL, infoLog);
+		TG_ERROR("Vertex Shader Compilation Failed: {}", infoLog);
+	}
+
+	const char* fragmentShaderSource =
+		"#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main(){\n"
+		"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n";
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, infoLog_L, NULL, infoLog);
+		TG_ERROR("Fragment Shader Compilation Failed: {}", infoLog);
+	}
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, infoLog_L, NULL, infoLog);
+		TG_ERROR("Shader program link Failed: {}", infoLog);
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		TGprocessInput(window);
-		glClearColor(1.0f, 0.5f, 0.5f, 1.0f);
+		glClearColor(0.2f, 0.4f, 0.6f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		glfwSwapBuffers(window);
 
+
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
